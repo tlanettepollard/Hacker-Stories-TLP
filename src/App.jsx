cd8333d96d692e4ae10aa4c69b985d90530201f7
T. Lanette <TLPollard4516@gmail.com>
2023-10-21 16:44:11 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 
+function getTitle(title) {
+  return title;
+}
+
 export default function App() {
+
   return (
     <div>
-      <h1>Hello React </h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search</label>
+      <input id='search' type="text" />
     </div>
   );
 }
